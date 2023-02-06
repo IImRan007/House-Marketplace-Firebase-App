1b888a07b67a8f9fa7038c0b00ebb89391a117c1
Mohd Imran <mohd78696321@gmail.com>
2023-02-06 11:41:40 +0530

    User signIn

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+// Firebase
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -21,12 +23,32 @@ const SignIn = () => {
     }));
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
         <header className="pageHeader">Welcome Back!</header>
         <main>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="email"
               className="emailInput"
